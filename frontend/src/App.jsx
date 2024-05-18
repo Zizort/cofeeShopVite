b1044ebbe0623fc8af9eea88a90aa3bddacc6019
Zizort <mtbox529@gmail.com>
2024-05-18 13:02:10 +0100

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+//to remove unused variables error: install npm install --save-dev eslint-plugin-react
+//and extends 'plugin:react/recommended', in eslintrc.cjs
---
+++ frontend/vite.config.js
@@ -0,0 +1,29 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// import the plugin
+import eslintPlugin from 'vite-plugin-eslint';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    eslintPlugin({
+      // setup the plugin
+      cache: false,
+      include: ['./src/**/*.js', './src/**/*.jsx'],
+      exclude: [],
+    }),
+  ],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000',
+      '/uploads': 'http://localhost:5000',
+    },
+  },
+});
